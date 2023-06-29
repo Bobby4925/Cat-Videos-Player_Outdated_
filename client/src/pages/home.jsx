74deff8e38ff5d44d3977c87b7fab1c1bcbd1bbc
Bobby4925 <122075721+Bobby4925@users.noreply.github.com>
2023-06-29 16:31:49 -0700

    It Works

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-
+import useSWR from 'swr'
 export default function Home() {
-  const [message, setMessage] = useState("");
-
+  const [message, setMessage] = useState();
   useEffect(() => {
-    fetch("http://localhost:8000/message")
+    fetch("http://localhost:8000/getvideos")
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+      .then((video) => setMessage(video[0].url))
   }, []);
-
   return (
     <>
     <div className="App">
-      <h1 className="text-center text-4xl">{message}</h1>
-      <h2>Hello</h2>
-      <button><Link to="/test">Test</Link></button>
+      <h1 className="text-center text-5xl pt-10">Cat Videos!</h1>
+      <iframe src={message} className="m-auto pt-52" width={700} height={700}></iframe>
     </div>
     </>
   );
 }
+
--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
-const express = require("express");
-const cors = require("cors");
+import express from 'express'
+import cors from 'cors'
 const app = express();
-
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 app.use(cors());
 app.use(express.json());
+
 
 app.get("/message", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+app.get("/getvideos",async (req,res) =>{
+  const length = await prisma.cat_Videos.count();
+  const num = Math.floor(Math.random() * length); 
+  console.log("Thing" + num)
+  const video = await prisma.cat_Videos.findMany({
+    where:{
+      id:num + 1, 
+    },
+    select:{
+      url: true
+    }
+  }
+  ); 
+  res.send(video);
+  console.log(video)
+});
+
 app.listen(8000, () => {
   console.log(`Server is running on port 8000.`);
 });
+
+/*
+select:{
+        url:true
+  }
+  */
